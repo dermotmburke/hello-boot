24d3fe2c166b22246f9956aec7e5ad4dd286dcda
dermot <dermotmburke@gmail.com>
2016-03-03 18:11:33 +0000

    Change the message

--- src/main/java/com/wiprodigital/hello/Application.java
+++ src/main/java/com/wiprodigital/hello/Application.java
@@ -39,7 +39,7 @@ public class Application {
 	@ResponseBody
 	public Map<String, String> env() {
         Map<String, String> map = new HashMap<String, String>();
-        map.put("greeting", "howdy");
+        map.put("greeting", "Hey, how's it going?");
         return map;
     }
 
