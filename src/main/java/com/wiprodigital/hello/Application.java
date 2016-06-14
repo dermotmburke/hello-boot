84890c628678f5e9cde0bc26c9b5c85682d46d31
dermot <dermotmburke@gmail.com>
2016-06-14 17:52:20 +0100

    rpm and swagger

--- src/main/java/com/wiprodigital/hello/Application.java
+++ src/main/java/com/wiprodigital/hello/Application.java
@@ -19,6 +19,7 @@ package com.wiprodigital.hello;
 import java.util.Map;
 import java.util.HashMap;
 
+import io.swagger.annotations.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -28,14 +29,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableAutoConfiguration
 @SpringBootApplication
 @Controller
+@EnableSwagger2
 public class Application {
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
+    @ApiOperation(value = "getGreeting", nickname = "getGreeting")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success", response = Map.class),
+            @ApiResponse(code = 401, message = "Unauthorized"),
+            @ApiResponse(code = 403, message = "Forbidden"),
+            @ApiResponse(code = 404, message = "Not Found"),
+            @ApiResponse(code = 500, message = "Failure")})
+    @RequestMapping(value = "/", method = RequestMethod.GET)
 	@ResponseBody
 	public Map<String, String> env() {
         Map<String, String> map = new HashMap<String, String>();
