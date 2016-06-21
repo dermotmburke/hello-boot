ab6b20c3bc5865047ba10f00faa497c5feb33024
dermotmburke <dermotmburke@gmail.com>
2016-06-21 13:39:45 +0100

    Update Application.java

--- src/main/java/com/wiprodigital/hello/Application.java
+++ src/main/java/com/wiprodigital/hello/Application.java
@@ -39,7 +39,7 @@ public class Application {
 	@ResponseBody
 	public Map<String, String> getGreeting() {
         Map<String, String> map = new HashMap<String, String>();
-        map.put("greeting", "Hi hows it going?");
+        map.put("greeting", "Hi'ya?");
         return map;
     }
 
