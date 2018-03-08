37b90d57161d572acde4f55140871099b5953273
Dermot Burke <dermotmburke@gmail.com>
2018-03-08 18:10:42 +0000

    get version form props

---
+++ pipelineUtils.sh
@@ -0,0 +1,65 @@
+
+setProperty() {
+  awk -v pat="^$1=" -v value="$1=$2" '{ if ($0 ~ pat) print value; else print $0; }' $3 > $3.tmp
+  mv $3.tmp $3
+}
+
+getProperty() {
+  PROP_KEY=$1
+  PROPERTY_FILE=$2
+  PROP_VALUE=`cat $PROPERTY_FILE | grep "$PROP_KEY" | cut -d'=' -f2`
+  echo $PROP_VALUE
+}
+
+incrementVersion() {
+  # Parse command line options.
+
+  while getopts ":Mmp" Option
+  do
+    case $Option in
+      M ) major=true;;
+      m ) minor=true;;
+      p ) patch=true;;
+    esac
+  done
+
+  shift $(($OPTIND - 1))
+
+  version=$1
+
+  # Build array from version string.
+
+  a=( ${version//./ } )
+
+  # If version string is missing or has the wrong number of members, show usage message.
+
+  if [ ${#a[@]} -ne 3 ]
+  then
+    echo "usage: $(basename $0) [-Mmp] major.minor.patch"
+    exit 1
+  fi
+
+  # Increment version numbers as requested.
+
+  if [ ! -z $major ]
+  then
+    ((a[0]++))
+    a[1]=0
+    a[2]=0
+  fi
+
+  if [ ! -z $minor ]
+  then
+    ((a[1]++))
+    a[2]=0
+  fi
+
+  if [ ! -z $patch ]
+  then
+    ((a[2]++))
+  fi
+
+  echo "${a[0]}.${a[1]}.${a[2]}"
+}
+
+"$@"
