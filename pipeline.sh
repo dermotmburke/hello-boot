d9c05a6a600042de27c8c0be2ecd1e1ab5013cd3
Travis CI <builds@travis-ci.com>
2018-03-12 15:48:40 +0000

    add branch condition for bump

--- pipeline.sh
+++ pipeline.sh
@@ -5,14 +5,15 @@ setUpGit() {
 
 buildAndBump() {
   gradle clean build
-  LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B | tr -d '\n')
-  if [[ "${LAST_COMMIT_MESSAGE}" == "${CD_COMMIT_MESSAGE}" ]];
+  if [[ "${TRAVIS_COMMIT_MESSAGE}" == "${CD_COMMIT_MESSAGE}" ]];
+  echo $TRAVIS_COMMIT_MESSAGE
   then
       echo "Skipping version bump"
   else
-    setUpGit
-    git checkout master
     if [[ $TRAVIS_BRANCH == 'master' ]]
+    then
+      setUpGit
+      git checkout master
       ./pipelineUtils.sh setProperty version $(./pipelineUtils.sh incrementVersion -p $(./pipelineUtils.sh getProperty version gradle.properties)) gradle.properties
       git add gradle.properties
       git commit -m "$CD_COMMIT_MESSAGE"
