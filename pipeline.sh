789f417bd05a87c157478575209529ff41a044a4
Travis CI <builds@travis-ci.com>
2018-03-12 15:38:49 +0000

    add branch condition for bump

--- pipeline.sh
+++ pipeline.sh
@@ -1,27 +1,28 @@
-setUp() {
+setUpGit() {
   git config --local user.name "Travis CI"
   git config --local user.email "builds@travis-ci.com"
 }
 
 buildAndBump() {
-  setUp
+  gradle clean build
   LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B | tr -d '\n')
   if [[ "${LAST_COMMIT_MESSAGE}" == "${CD_COMMIT_MESSAGE}" ]];
   then
-      echo "Skipping build"
+      echo "Skipping version bump"
   else
+    setUpGit
     git checkout master
-    gradle clean build
-    ./pipelineUtils.sh setProperty version $(./pipelineUtils.sh incrementVersion -p $(./pipelineUtils.sh getProperty version gradle.properties)) gradle.properties
-    git add gradle.properties
-    git commit -m "$CD_COMMIT_MESSAGE"
-    git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dermotmburke/hello-boot.git master
+    if [[ $TRAVIS_BRANCH == 'master' ]]
+      ./pipelineUtils.sh setProperty version $(./pipelineUtils.sh incrementVersion -p $(./pipelineUtils.sh getProperty version gradle.properties)) gradle.properties
+      git add gradle.properties
+      git commit -m "$CD_COMMIT_MESSAGE"
+      git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dermotmburke/hello-boot.git master
+    fi
   fi
-
 }
 
 prepareDeploy() {
-  setUp
+  setUpGit
   export RELEASE_FILE=build/libs/hello-boot-$TRAVIS_TAG.jar
   git tag "$TRAVIS_TAG"
   mv build/publications/mavenJava/pom-default.xml build/publications/mavenJava/pom.xml
