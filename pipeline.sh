cb7e31f71efbcf826ed6f7eb0a241f1a310a6fb5
Dermot Burke <dermotmburke@gmail.com>
2018-03-12 13:29:09 +0000

    [travis] Bump version number

--- pipeline.sh
+++ pipeline.sh
@@ -1,7 +1,7 @@
 buildAndBump() {
   gradle clean build
   ./pipelineUtils.sh setProperty version $(./pipelineUtils.sh incrementVersion -p $(./pipelineUtils.sh getProperty version gradle.properties)) gradle.properties
-  git commit -am "Bump version number"
+  git commit -am "$CD_COMMIT_MESSAGE"
   git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dermotmburke/hello-boot.git master
 }
 
