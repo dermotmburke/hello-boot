e792299106cb2f021d02f7dcfe62690b5d45b783
Dermot Burke <dermotmburke@gmail.com>
2018-03-12 14:14:59 +0000

    Add gradle.properties rather than all

--- pipeline.sh
+++ pipeline.sh
@@ -6,7 +6,8 @@ buildAndBump() {
   else
     gradle clean build
     ./pipelineUtils.sh setProperty version $(./pipelineUtils.sh incrementVersion -p $(./pipelineUtils.sh getProperty version gradle.properties)) gradle.properties
-    git commit -am "$CD_COMMIT_MESSAGE"
+    git add gradle.properties
+    git commit -m "$CD_COMMIT_MESSAGE"
     git push https://$GITHUB_USER:$GITHUB_TOKEN@github.com/dermotmburke/hello-boot.git master
   fi
 
